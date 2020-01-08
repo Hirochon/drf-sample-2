543b8ace1241e2874269f49baa48dd94b18219e4
Hirochon <heacet43@gmail.com>
2020-01-09 00:11:43 +0900

    [add] Add Shop/models.py

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,18 @@
+import uuid
 from django.db import models
+from django.utils import timezone
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Book(models.Model):
+    """本モデル"""
+
+    class Meta:
+        db_table = 'book'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(verbose_name='タイトル', max_length=20)
+    price = models.IntegerField(verbose_name='価格', null=True, validators=[MinValueValidator(1)])
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
