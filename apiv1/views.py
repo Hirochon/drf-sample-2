88548f4608dce49fd7ffcd04e3c6cd631f36b597
Hirochon <heacet43@gmail.com>
2020-01-09 00:11:05 +0900

    [add] apiv1/views.py

--- apiv1/views.py
+++ apiv1/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from shop.models import Book
+from .serializer import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    """本モデルのCRUD用APIクラス"""
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
